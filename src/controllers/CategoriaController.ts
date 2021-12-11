24f547171f5be30785552bb520e05b7746ee9630
Milton Mendes <miltonsergiom@gmail.com>
2021-12-11 02:01:43 -0300

    Added a new function to get products by category

--- src/controllers/CategoriaController.ts
+++ src/controllers/CategoriaController.ts
@@ -9,5 +9,13 @@ export default {
 
         return response.status(200).json({ data: result });
     },
+    
+    async findByCategory(request: Request, response: Response) {
+        const { id_category } = request.params;
+        const product = await knex('tb_products').where({ id_category });
+
+        return response.status(200).json({ data: product });
+
+    },
 
 }
